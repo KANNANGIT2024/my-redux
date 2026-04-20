3cda8f178e6c89f4802e85e0dd076a59149fb34e
kannan <kanannaes@gmail.com>
2026-04-20 15:20:45 +0530

    Restructure project files

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from "react";
 import { fetchUsers } from "@/features/user/userSlice";
-import { useAppDispatch, useAppSelector } from "../lib/hooks";
+import { useAppDispatch, useAppSelector } from "../src/lib/hooks";
 
 import {
   Table,
